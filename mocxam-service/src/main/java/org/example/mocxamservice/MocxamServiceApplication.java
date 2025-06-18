b4941a7bfcb36e42b616635ae6901b267f0b0cc2
Nguyễn Trí Khôi <74496543+killdyblack@users.noreply.github.com>
2025-06-18 21:08:58 +0700

    Khôi Up Mocxam-service

---
+++ mocxam-service/src/main/java/org/example/mocxamservice/MocxamServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.mocxamservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MocxamServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MocxamServiceApplication.class, args);
+	}
+
+}
---
+++ mocxam-service/src/test/java/org/example/mocxamservice/MocxamServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mocxamservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MocxamServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
