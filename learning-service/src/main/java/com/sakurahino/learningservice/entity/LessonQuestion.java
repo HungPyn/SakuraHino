3b7a04b16f2be04e856ae0c5949f82cab391d697
vuonngg <hoangvanvuong2974@gmail.com>
2025-08-17 11:58:38 +0700

    doi ti port

--- learning-service/src/main/java/com/sakurahino/learningservice/entity/LessonQuestion.java
+++ learning-service/src/main/java/com/sakurahino/learningservice/entity/LessonQuestion.java
@@ -4,6 +4,8 @@ import com.sakurahino.learningservice.enums.QuestionType;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -46,7 +48,7 @@ public class LessonQuestion {
     private Lesson lesson;
 
     @NotNull
-    @Lob
+    @Enumerated(EnumType.STRING)
     @Column(name = "question_type", nullable = false)
     private QuestionType questionType;
 
